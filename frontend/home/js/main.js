84283888c3ee7f5af4365ce6d7c94f9011395b93
tritone11 <gioele@officinetesla.it>
2015-10-11 21:32:20 +0200

    main update

--- frontend/home/js/main.js
+++ frontend/home/js/main.js
@@ -1,9 +1,24 @@
 var cartPosition=450;
 function addProduct(p){
-    var element = document.createElement("div");
-    element.appendChild(document.createTextNode(p));
+    if(cartPosition!=0){
+        var element = document.createElement("div");
+        var name = document.createElement("div");
+    name.appendChild(document.createTextNode(p));
+    name.className='flag-text';
     element.className='flag';
+        element.appendChild(name);
     document.getElementById('bought-container').appendChild(element);
-    cartPosition-=25;
+    cartPosition-=25;}
+    if(cartPosition!=0){
     document.getElementById('basket').style.top='-'+cartPosition+'px';
+    }else{
+        document.getElementById('else').style.display='block'
+    }
+}
+function checkMobile(){
+    if(innerWidth<innerHeight){
+        document.getElementsByClassName('products-container')[0].style.width='90%'
+    }else{
+        document.getElementsByClassName('products-container')[0].style.width='50%'
+    }
 }
