19ac551f41f476ec1a3f54f2d96d885f572a1e2d
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-10-12 20:09:21 +0200

    added5

--- frontend/app.js
+++ frontend/app.js
@@ -50,7 +50,7 @@ app.get('/get_product', function (req, res) {
   var pgnum=req.query.pgnum||-1;
   res.myxml='<?xml version="1.0" encoding="UTF-8"?><response>';
   res.set('Content-Type', 'text/xml');
-  doit(1,function(){doit(2,function(){doit(3,function(){doit(4,function(){res.send(res.myxml+'</response>');},res);},res);},res);},res); 
+  doit(1,function(){doit(2,function(){doit(3,function(){doit(4,function(){doit(5,function(){res.send(res.myxml+'</response>');},res);},res);},res);},res);},res); 
 });
 doit=function(m_id,onend,res){var p=3001+(m_id*10);
 	MongoClient.connect('mongodb://localhost:'+p+'/meteor', function(err, db) {
