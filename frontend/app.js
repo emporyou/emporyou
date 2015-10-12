9ef83fbe63c38fac72f6dea64f9335039477a192
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-10-12 20:23:21 +0200

    added5 2

--- frontend/app.js
+++ frontend/app.js
@@ -7,7 +7,12 @@ var endOfLine = require('os').EOL;
 //---------------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------ EXAMPLES
-app.get('/', function (req, res) { res.send('Hello World!!');});
+app.get('/', function (req, res) {
+	app.get('/', function(req, res){
+    res.sendfile('index.html', { root: __dirname + "/frontend" } );
+});
+	
+});
 app.post('/', function (req, res) {res.send('Got a POST request');});
 app.put('/user', function (req, res) { res.send('Got a PUT request at /user');});
 app.delete('/user', function (req, res) {res.send('Got a DELETE request at /user');});
