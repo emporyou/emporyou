c3ae9e16aa6d834098a4eccc2c6aae965ba6c746
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-10-14 00:40:07 +0200

    syncrender

--- frontend/app.js
+++ frontend/app.js
@@ -107,8 +107,8 @@ app.get('/get_product', function (req, res) {
 doit=function(m_id,onend,res,q){var p=3001+(m_id*10);
 	MongoClient.connect('mongodb://localhost:'+p+'/meteor', function(err, db) {
 		db.collection('Products').find(q).toArray(function(err,docs){
-			res.myxml+="<merchant_products><name>Merchant "+m_id+"</name><id>"+m_id+"</id><lat>2</lat><lon>2</lon><city>Milano</city>";
-			res.myxml+=o2xml('product',docs)+'</merchant_products>';
+			var x="</product><merchant><name>Merchant "+m_id+"</name><id>"+m_id+"</id><lat>2</lat><lon>2</lon><city>Milano</city></merchant>";
+			res.myxml+=ooo.sub(o2xml('product',docs),'</product>',x);
 			db.close();try{onend();}catch(ex){res.send(res.myxml+'</response>');}
 });});};
 //---------------------------------------------------------------------------------------------------
