d068e9d4a860523d0e03b1c6ae4f6f27631e2fd7
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-10-13 04:34:47 +0200

    fix

--- frontend/app.js
+++ frontend/app.js
@@ -61,7 +61,7 @@ app.get('/get_product_image', function (req, res) {
 	var q_exp='metadata.productId';var q_field=p_id;
 	if(v_id>-1){q_exp='metadata.variantId';q_field=v_id;}
 	MongoClient.connect('mongodb://localhost:'+p+'/meteor', function(err, db) {
-		db.collection('cfs_gridfs.'+isize+'.files').find({metadata.productId:p_id}).toArray(function(err,docs){
+		db.collection('cfs_gridfs.'+isize+'.files').find({'metadata.productId':p_id}).toArray(function(err,docs){
 			if(docs.length>0){if(priority>docs.length){priority=docs.length}
 				res.set('Content-Type', docs[priority].contentType);
 				db.collection('cfs_gridfs.'+isize+'.chunks').find({'files_id':docs[priority]._id}).toArray(function(err,docs){
