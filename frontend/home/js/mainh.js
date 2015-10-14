09a5c7bb0a4fcb8c21303da55b9e2d53cfcf8e3e
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-10-14 02:07:02 +0200

    syncrender

--- frontend/home/js/mainh.js
+++ frontend/home/js/mainh.js
@@ -1,8 +1,10 @@
-cartemplatepreloaded=false;
 rendercart=function(){
-	if(!cartemplatepreloaded){ooo.preload('products-cart.xml',function(){cartemplatepreloaded=true;rendercart();})}
-	var cartdata=document.getElementById('cart-data');
-	var cartXML='<?xml version="1.0" encoding="UTF-8"?><response>';
-	cartXML+='</response>';
-	ooo.syncrender('products-cart-target',ooo.parsexml(cartXML).documentElement,ooo.preloaded('products-cart.xml'));
-};
+	if(!cartemplatepreloaded){ooo.preload('products-cart.xml',function(){window.cartemplatepreloaded=true;setTimeout('rendercart();',250)})}
+	else{
+	var cartdata=ooo.sel("//div[@id='cart-data']/textarea",document);
+	if(cartdata.length>0){
+		var cartXML='<?xml version="1.0" encoding="UTF-8"?><response>';
+		for(var c=0;c<cartdata.length;c++){cartXML+='<product>'+cartdata[c].value+'</product>';}cartXML+='</response>';
+		var fff=ooo.parsexml(cartXML);
+		ooo.syncrender('products-cart-target',fff.documentElement,ooo.preloaded('products-cart.xml').documentElement);
+}	}	};
