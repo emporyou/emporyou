16f6805cc65404c42366288ea3b9c08ae7f59a28
tritone11 <gioele@officinetesla.it>
2015-10-16 01:50:26 +0200

    aaa

--- frontend/home/js/main.js
+++ frontend/home/js/main.js
@@ -53,14 +53,12 @@ function closeProduct(){
 }
 function checkMobile(){
     if(innerWidth<innerHeight){
-        document.getElementsByClassName('products-container')[0].style.width='90%';
         document.getElementsByClassName('product-info-main')[0].style.width='90%';
         document.getElementsByClassName('ics')[0].style.display="none";
         if(set==1){document.getElementsByClassName('google-map')[0].style.width='87%';}
         //document.getElementById('basket-container').style.display="none";
         document.getElementById('header-items-container').style.display="none";
     }else{
-        document.getElementsByClassName('products-container')[0].style.width='73%';
         document.getElementsByClassName('product-info-main')[0].style.width='60%';
         document.getElementsByClassName('ics')[0].style.display="";
         if(set==1){document.getElementsByClassName('google-map')[0].style.width='49%';}
