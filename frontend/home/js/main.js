0732027d20d93eecf908d59a09503506f8f95ca7
tritone11 <gioele@officinetesla.it>
2015-10-26 18:20:44 +0100

    aaa

--- frontend/home/js/main.js
+++ frontend/home/js/main.js
@@ -1,5 +1,4 @@
 var cartPosition=450;var total=0;var set=0;var cartOpened=0;var productOpened=0;
-window.gClass=document.getElementsByClassName;
 function addProduct(productName,XMLid,PRDid){
    var elm=document.getElementById(PRDid);
     if(elm){
@@ -16,7 +15,7 @@ function addProduct(productName,XMLid,PRDid){
     element.className='flag';
         element.appendChild(name);
     document.getElementById('bought-container').appendChild(element);
-    cartPosition-=25;}else{gClass('flag-text')[0].innerHTML=total.toString();
+    cartPosition-=25;}else{document.getElementsByClassName('flag-text')[0].innerHTML=total.toString();
                           };
     if(cartPosition!=0){
     document.getElementById('basket').style.top='-'+cartPosition+'px';
@@ -36,34 +35,34 @@ function openProduct(event,elm,merid,id){closeCart();
     if(set==1){openMap()}
     //var elm=document.getElementById('openProd');
 if(event.srcElement==elm){
-    gClass('products-main')[0].style.display="none";
-    gClass('emporyoum-bar')[0].style.top="13%";
-    gClass('emporyoum-bar')[0].style.width="25%";
+    document.getElementsByClassName('products-main')[0].style.display="none";
+    document.getElementsByClassName('emporyoum-bar')[0].style.top="13%";
+    document.getElementsByClassName('emporyoum-bar')[0].style.width="25%";
     //document.getElementById('basket-container').style.display="none";
-    gClass('products-page-main')[0].classList.remove('hidden');
-    gClass('products-page-main')[0].style.top=('13%');
+    document.getElementsByClassName('products-page-main')[0].classList.remove('hidden');
+    document.getElementsByClassName('products-page-main')[0].style.top=('13%');
     
 }
 }
 function closeProduct(){
     productOpened=0;
-    gClass('products-main')[0].style.display="";
-   gClass('emporyoum-bar')[0].style.top="14%";
-    gClass('emporyoum-bar')[0].style.width="100%";
+    document.getElementsByClassName('products-main')[0].style.display="";
+   document.getElementsByClassName('emporyoum-bar')[0].style.top="14%";
+    document.getElementsByClassName('emporyoum-bar')[0].style.width="100%";
     //document.getElementById('basket-container').style.display="";
-    gClass('products-page-main')[0].classList.add('hidden');
+    document.getElementsByClassName('products-page-main')[0].classList.add('hidden');
 }
 function checkMobile(){
     if(innerWidth<innerHeight){
-        //gClass('product-info-main')[0].style.width='90%';
-        gClass('ics')[0].style.display="none";
-        //if(set==1){gClass('google-map')[0].style.width='87%';}
+        //document.getElementsByClassName('product-info-main')[0].style.width='90%';
+        document.getElementsByClassName('ics')[0].style.display="none";
+        //if(set==1){document.getElementsByClassName('google-map')[0].style.width='87%';}
         //document.getElementById('basket-container').style.display="none";
         //document.getElementById('header-items-container').style.display="none";
     }else{
-        gClass('product-info-main')[0].style.width='60%';
-        gClass('ics')[0].style.display="";
-        if(set==1){gClass('google-map')[0].style.width='61.5%';}
+        document.getElementsByClassName('product-info-main')[0].style.width='60%';
+        document.getElementsByClassName('ics')[0].style.display="";
+        if(set==1){document.getElementsByClassName('google-map')[0].style.width='61.5%';}
         //document.getElementById('basket-container').style.display="";
         document.getElementById('header-items-container').style.display="flex";
     }
@@ -72,30 +71,30 @@ window.ismaploadedonce=false;
 function openMap(){
     if(productOpened==0){
     if(set==0){
-    gClass('google-map')[0].classList.remove('transformed');
-    gClass('google-map-container')[0].classList.add('google-map-container-opened');
-    gClass('emporyoum-bar')[0].style.top="13%";
-        gClass('emporyoum-bar')[0].style.left="-17%";
-        gClass('products-main')[0].style.top="55%";
-    gClass('emporyoum-bar')[0].style.width="25%";
-    if(cartOpened==1){gClass('google-map-container')[0].style.top="58%"}else{
-            gClass('google-map-container')[0].style.top="13%"};
+    document.getElementsByClassName('google-map')[0].classList.remove('transformed');
+    document.getElementsByClassName('google-map-container')[0].classList.add('google-map-container-opened');
+    document.getElementsByClassName('emporyoum-bar')[0].style.top="13%";
+        document.getElementsByClassName('emporyoum-bar')[0].style.left="-17%";
+        document.getElementsByClassName('products-main')[0].style.top="55%";
+    document.getElementsByClassName('emporyoum-bar')[0].style.width="25%";
+    if(cartOpened==1){document.getElementsByClassName('google-map-container')[0].style.top="58%"}else{
+            document.getElementsByClassName('google-map-container')[0].style.top="13%"};
 			set=1;if(!window.ismaploadedonce){
 				ooo.ins(document.getElementsByTagName('head')[0],'script',['type','text/javascript','src','https://maps.googleapis.com/maps/api/js?fg=0&callback=initMap']);
 				setTimeout('initMap()',500);
 		}}else{
-            gClass('products-main')[0].style.top="25%";
-    gClass('google-map')[0].classList.add('transformed');
-    gClass('google-map-container')[0].classList.remove('google-map-container-opened');
-    gClass('emporyoum-bar')[0].style.top="14%";
-            gClass('emporyoum-bar')[0].style.left="";
-    gClass('emporyoum-bar')[0].style.width="100%";set=0;
+            document.getElementsByClassName('products-main')[0].style.top="25%";
+    document.getElementsByClassName('google-map')[0].classList.add('transformed');
+    document.getElementsByClassName('google-map-container')[0].classList.remove('google-map-container-opened');
+    document.getElementsByClassName('emporyoum-bar')[0].style.top="14%";
+            document.getElementsByClassName('emporyoum-bar')[0].style.left="";
+    document.getElementsByClassName('emporyoum-bar')[0].style.width="100%";set=0;
     }  
 }}
 function openCart(){
     if(openCart==1){closeCart()}else{
     scrollTo(document.body, 0, 200);
-    gClass('products-main')[0].style.top="55%";
+    document.getElementsByClassName('products-main')[0].style.top="55%";
     document.getElementById('basket-container').style.top="-50%";
     setTimeout('cartReady()',400);
 	
@@ -103,9 +102,9 @@ function openCart(){
 }}
 function cartReady(){
     cartOpened=1;ooo.$$('products-cart-target').style.display='';//SHOW the cart
-    //gClass('emporyoum-bar')[0].style.top="75%";
-    //gClass('products-main')[0].style.top="100%";
-    //gClass('google-map-container')[0].style.top="58%";
+    //document.getElementsByClassName('emporyoum-bar')[0].style.top="75%";
+    //document.getElementsByClassName('products-main')[0].style.top="100%";
+    //document.getElementsByClassName('google-map-container')[0].style.top="58%";
     document.getElementById('product-page-target').style.top="58%";
     document.getElementById('products-cart-target').style.height="48%";
     document.getElementById('pattern').style.bottom="0";
@@ -124,10 +123,10 @@ function scrollTo(element, to, duration) {
 function defaultPage(){
     productOpened=0;
     if(cartOpened==1){closeCart()};
-    if(set==1){openMap()}else{gClass('google-map-container')[0].style.top="13%"}
-    gClass('products-page-main')[0].classList.add('hidden');
-    gClass('products-main')[0].style.top="25%";
-    gClass('products-main')[0].style.display="block";
+    if(set==1){openMap()}else{document.getElementsByClassName('google-map-container')[0].style.top="13%"}
+    document.getElementsByClassName('products-page-main')[0].classList.add('hidden');
+    document.getElementsByClassName('products-main')[0].style.top="25%";
+    document.getElementsByClassName('products-main')[0].style.display="block";
     document.getElementById('product-page-target').style.top="58%";
     document.getElementById('basket-container').style.top="0";
     scrollTo(document.body, 0, 200);
@@ -136,7 +135,7 @@ function closeCart(){
     ooo.$$('products-cart-target').style.display='none';
     if(document.getElementById('pattern')){
     cartOpened=0;
-    gClass('emporyoum-bar')[0].style.top="14%";
+    document.getElementsByClassName('emporyoum-bar')[0].style.top="14%";
     ooo.$$('products-cart-target').style.display='none';
     document.getElementById('products-cart-target').style.height="0";
     document.getElementById('pattern').style.bottom="150px";}
@@ -153,11 +152,11 @@ function shareDialog(elm){
 }
 function openCat(e){
     for(i=0;i<10;i++){
-        if(gClass('search')[i]){
-            gClass('cat-cont')[i].style.height="50px";
-            gClass('cat-cont')[i].style.display="50px";
-            gClass('search')[i].style.height="0";
-            gClass('search')[i].style.display="none";
+        if(document.getElementsByClassName('search')[i]){
+            document.getElementsByClassName('cat-cont')[i].style.height="50px";
+            document.getElementsByClassName('cat-cont')[i].style.display="50px";
+            document.getElementsByClassName('search')[i].style.height="0";
+            document.getElementsByClassName('search')[i].style.display="none";
         }
     }
     e.style.height="25px";
