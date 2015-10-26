f42224a4fbf3dc6db4212f108f7a60279486e14a
tritone11 <gioele@officinetesla.it>
2015-10-26 17:59:12 +0100

    aaa

--- frontend/home/js/main.js
+++ frontend/home/js/main.js
@@ -152,5 +152,6 @@ function shareDialog(elm){
 }
 function cityCat(){
     document.getElementById('cat-city').style.height="25px";
-    document.getElementById('city-search').style.height="50px"
+    document.getElementById('city-search').style.height="50px";
+    document.getElementById('city-search').style.display=""
 }
