500d81418364d46d05ca657dda31d472f75998c6
tritone11 <gioele@officinetesla.it>
2015-10-30 23:20:17 +0100

    aaa

--- frontend/home/js/bookmarklet.js
+++ frontend/home/js/bookmarklet.js
@@ -1,11 +1,12 @@
 var myForm=document.createElement('form');
 var myInput=document.createElement('input');
 myInput.setAttribute('type','hidden');
-myInput.setAttribute('id','jsondata');
 myInput.setAttribute('name','jsondata')
 myInput.setAttribute('value','JSON.stringify(selectedProduct())');
 myForm.setAttribute('action','http://emporyou.com/metaframe?page=vendor/product-edit.xml');
 myForm.setAttribute('method','post');
+myForm.setAttribute('id','jsondata');
+myForm.setAttribute('name','jsondata');
 myForm.setAttribute('target','_blank');
 myForm.setAttribute('enctype','multipart/form-data');
 myForm.style.display="none";
