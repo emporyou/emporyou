00ee278160a87b7c5225c4dfdc871c4de3fc0280
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-11-02 00:15:42 +0100

    images

--- frontend/app.js
+++ frontend/app.js
@@ -196,6 +196,7 @@ servenoimage=function(res){res.sendFile('/root/emporyou/frontend/img/default-pro
 app.use(function(req,res,next){
 	        if(req.originalUrl.indexOf('/admin' )==0){if(!req.isAuthenticated()){res.redirect('../login.html')}else{express.static('./')(req,res,next)}}
 		else if(req.originalUrl.indexOf('/vendor')==0){console.log('gio');if(!req.isAuthenticated()){res.redirect('../login.html')}else{express.static('./')(req,res,next)}}
+		else if(req.originalUrl.indexOf('/uploads')==0){console.log('gio');express.static('./')(req,res,next)}}
 		   else{express.static('./home')(req,res,next)}}
 );
 var PORT=80;
