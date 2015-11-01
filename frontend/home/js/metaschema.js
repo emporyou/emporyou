cce7783ccbc6293fa78b67b7c163bfc10f61f384
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-11-02 00:56:31 +0100

    images

--- frontend/home/js/metaschema.js
+++ frontend/home/js/metaschema.js
@@ -94,7 +94,7 @@ $.v2xml=function(n,v){if(typeof(v)=='function'){return ''}var cd=false;if(typeof
     var prel;for(var xi=0;xi<ins.length;xi++){prel=ooo.xatt(ins[xi],'preload');if(!$.preloaded(prel)){flag=true;break;}}}}
     //TODO:Check inline's inlines
     if(flag){$.preload(prel,$._dorendercontrol2,oo);}else{$.load(oo.data,oo.elm,$._dorendercontrol3,false,false,oo);}};
- $._dorendercontrol3=function(req,oo){var ct=req.getResponseHeader('content-type');if(!ct){if(req.responseText.indexOf('<?xml version="1')<20){ct='xml'}}if(!ct){ct='json'}if(ct.indexOf('xml')<0){oo.dataXML=JSON.parse(req.responseText)}else{oo.dataXML=ooo.parsexml(req.responseText);}window.lastJob=oo;var s=$.syncrender(oo.target,oo.templateXML.documentElement,oo.dataXML.documentElement,oo.mode||'normal');if(oo.onfinish){oo.onfinish(s,oo)}};
+ $._dorendercontrol3=function(req,oo){var ct=req.getResponseHeader('content-type');if(!ct){if(req.responseText.indexOf('<?xml version="1')<20){ct='xml'}}if(!ct){ct='json'}if(ct.indexOf('xml')<0){oo.dataXML=JSON2xmldoc(JSON.parse(req.responseText))}else{oo.dataXML=ooo.parsexml(req.responseText);}window.lastJob=oo;var s=$.syncrender(oo.target,oo.templateXML.documentElement,oo.dataXML.documentElement,oo.mode||'normal');if(oo.onfinish){oo.onfinish(s,oo)}};
 /*--------------------------------------------------------------------------------------------  XML RENDER MAIN FUNCTION */
 /*THIS is the most functional js function in this script - quite everything else is to make THIS possible.
 normally use syncrender, CALLING THIS FUNCTION DIRECTLY LOSES COMPATIBILITY WITH JSON (you have to .JSON2xmldoc(yourJSONresponse) by yourself)
