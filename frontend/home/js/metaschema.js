5aaebe09b5e8abc419d13924cf1a6d67a89024e2
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-11-02 01:27:16 +0100

    images

--- frontend/home/js/metaschema.js
+++ frontend/home/js/metaschema.js
@@ -94,13 +94,10 @@ $.v2xml=function(n,v){if(typeof(v)=='function'){return ''}var cd=false;if(typeof
     var prel;for(var xi=0;xi<ins.length;xi++){prel=ooo.xatt(ins[xi],'preload');if(!$.preloaded(prel)){flag=true;break;}}}}
     //TODO:Check inline's inlines
     if(flag){$.preload(prel,$._dorendercontrol2,oo);}else{$.load(oo.data,oo.elm,$._dorendercontrol3,false,false,oo);}};
- $._dorendercontrol3=function(req,oo){var ct=req.getResponseHeader('content-type');if(!ct){if(req.responseText.indexOf('<?xml version="1')<20){ct='xml'}}if(!ct){ct='json'}if(ct.indexOf('xml')<0){
-	 
-	 console.log(req.responseText);
+ $._dorendercontrol3=function(req,oo){console.log('1'+req.responseText);var ct=req.getResponseHeader('content-type');if(!ct){if(req.responseText.indexOf('<?xml version="1')<20){ct='xml'}}if(!ct){ct='json'}ct=json;if(ct.indexOf('xml')<0){
+	 console.log('2'+req.responseText);
 	 oo.dataXML=ooo.JSON2xmldoc(JSON.parse(req.responseText));
-	 if(ooo.ixml(oo.dataXML).indexOf("Below")>-1){throw new Error('here')}
-	 
-	 
+	 if(ooo.ixml(oo.dataXML).indexOf("Below")>-1){throw new Error('here')}	 
 	 }else{oo.dataXML=ooo.parsexml(req.responseText);}window.lastJob=oo;var s=$.syncrender(oo.target,oo.templateXML.documentElement,oo.dataXML.documentElement,oo.mode||'normal');if(oo.onfinish){oo.onfinish(s,oo)}};
 /*--------------------------------------------------------------------------------------------  XML RENDER MAIN FUNCTION */
 /*THIS is the most functional js function in this script - quite everything else is to make THIS possible.
