245c57a556a1ca46fca8caa02fb7a7479c2cf49b
tritone11 <gioele@officinetesla.it>
2015-11-02 01:33:46 +0100

    adding mio

--- frontend/home/js/main.js
+++ frontend/home/js/main.js
@@ -32,7 +32,7 @@ function addProduct(productName,XMLid,PRDid){
 function openProduct(event,elm,merid,id){closeCart();
     if(set==1){openMap();}
     productOpened=1;
-    ooo.render('product-page-target','product-page-template.xml','http://emporyou.com/get_product?m_id='+merid+'&p_id='+id,false,false,checkMobile);
+    ooo.render('product-page-target','product-page-template.xml','http://emporyou.com/get_deal?m_id='+merid+'&p_id='+id,false,false,checkMobile);
     if(set==1){openMap()}
     //var elm=document.getElementById('openProd');
 if(event.srcElement==elm){
