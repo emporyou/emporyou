a33b8fcaffadcbce036812803aca7ea42c5aacbd
tritone11 <gioele@officinetesla.it>
2015-11-10 00:52:49 +0100

    editing coupon

--- merchant/serialize.js
+++ merchant/serialize.js
@@ -1,67 +1,75 @@
-window.uno = {};
-uno.xml = {};
-uno.xml.test = function (f) {
-    window.o = {};
+if(!window.ooo)window.ooo = {};
+ooo.form2JSON = function (f) {
+    var o = {};
     if (f.getElementsByTagName('fieldset')[0]) {
         var fld = f.getElementsByTagName('fieldset');
-        var isArr;
+        var isArr;var isNotArr;
         window.arrFld = [];
         for (i = 0; i < fld.length; i++) {
-            var idd = fld[i].id;
+            var idd = fld[i].name;
             if (idd.indexOf('[]') > -1) {
                 isArr = true;
-                console.log('is array');
-                arrFld.push(fld[i])
-            }
-        }
-        if (isArr != true) {
+                arrFld.push(fld[i]);
+            }else{isNotArr=true;}
+        };
+        if (isArr != true || isNotArr==true) {
             for (i = 0; i < fld.length; i++) {
                 var fldName = fld[i].name;
                 var fldObj = {};
                 var inp = fld[i].getElementsByTagName('input');
                 var t = fld[i].getElementsByTagName('textarea');
-                for (z = 0; z < inp.length; z++) {
+                var s = f.getElementsByTagName('select');
+                if(inp.length>0){for (z = 0; z < inp.length; z++) {
                     var b = inp[z].name;
                     var c = inp[z].value
                     fldObj[b] = c;
-                }
-                for (z = 0; z < t.length; z++) {
+                }}
+                if(t.length>0){for (z = 0; z < t.length; z++) {
                     var b = t[z].name;
                     var c = t[z].value
                     fldObj[b] = c;
-                }
+                }}
+                if(s.length>0){for (z = 0; z < s.length; z++) {
+                        var b = s[z].name;
+                        var c = s[z].value
+                        fldObj[b] = c;
+                    }}
                 o[fldName] = fldObj
             }
-        } else {
+        };if(isArr == true){
             for (i = 0; i < arrFld.length; i++) {
                 var arrName = arrFld[i].name;
                 var af = arrFld[i].getElementsByTagName('fieldset');
-                for (i = 0; i < af.length; i++) {
-                    var fldName = af[i].name;
+                for (r = 0; r < af.length; r++) {
+                    var fldName = af[r].name;
                     var arrObj = {};
-                    var inp = af[i].getElementsByTagName('input');
-                    var t = af[i].getElementsByTagName('textarea');
-                    for (z = 0; z < inp.length; z++) {
+                    var inp = af[r].getElementsByTagName('input');
+                    var t = af[r].getElementsByTagName('textarea');
+                    var s = f.getElementsByTagName('select');
+                    if(inp.length>0){for (z = 0; z < inp.length; z++) {
                         var b = inp[z].name;
                         var c = inp[z].value
                         arrObj[b] = c;
-                    }
-                    for (z = 0; z < t.length; z++) {
+                    }}
+                    if(s.length>0){for (z = 0; z < s.length; z++) {
+                        var b = s[z].name;
+                        var c = s[z].value
+                        arrObj[b] = c;
+                    }}
+                    if(t.length>0){for (z = 0; z < t.length; z++) {
                         var b = t[z].name;
                         var c = t[z].value
                         arrObj[b] = c;
-                    }
-                    if (!o[arrName]) {
-                        o[arrName] = []
-                    }
+                    }}
+                    o[arrName] = [];
                     o[arrName].push(arrObj);
                 }
-
             }
         }
     } else {
         var t = f.getElementsByTagName('textarea');
         var i = f.getElementsByTagName('input');
+        var s = f.getElementsByTagName('select');
         var g = function (a) {
             for (var i = 0; i < a.length; i++) {
                 var v = a[i];
@@ -75,4 +83,5 @@ uno.xml.test = function (f) {
         g(t);
         g(i);
     }
+    return o;
 }
