92d5f8d151a1140e5efb10ab86016dc72423760c
tritone11 <gioele@officinetesla.it>
2015-11-09 20:59:43 +0100

    editing coupon

--- merchant/serialize.js
+++ merchant/serialize.js
@@ -1,30 +1,19 @@
 window.uno={};
 uno.xml={};
-uno.xml._collect = function (a, f) {
+uno.xml._add = function (a) {
     var n = [];
     for (var i = 0; i < a.length; i++) {
-        var v = f(a[i]);
+        var v = a[i];
         if (v != null) {
             n.push(v);
         }
     }
     return n;
 };
-uno.xml._serialize = function (f) {
-    var g = function (n) {
-        return f.getElementsByTagName(n)
-    };
-    var nv = function (e) {
-        if (e.name) return encodeURIComponent(e.name) + '=' + encodeURIComponent(e.value);
-        else return ''
-    };
-    var c1 = function (i) {
-        if ((i.type != 'radio' && i.type != 'checkbox') || i.checked) return nv(i)
-    };
-    var i = uno.xml._collect(g('input'), c1);
-    var s = uno.xml._collect(g('select'), nv);
-    var t = uno.xml._collect(g('textarea'), nv);
-    return i.concat(s).concat(t).join('&');
-};
 
-uno.xml._serialize(document.getElementById('coupon-form'))
+uno.xml.test = function (f){
+    window.o={};
+    var i = uno.xml._add(f.getElementsByTagName('input'));console.log(i);
+    var fld = uno.xml._add(f.getElementsByTagName('fieldset'));console.log(fld);
+    o.field=fld;
+}
