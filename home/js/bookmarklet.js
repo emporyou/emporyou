01e6a8fd4cde006d407dcbd14c16173d3ffc7bb5
tritone11 <gioele@officinetesla.it>
2015-11-11 15:58:46 +0100

    aaa

--- home/js/bookmarklet.js
+++ home/js/bookmarklet.js
@@ -8,20 +8,32 @@ if(emporyoumode=='reaction'){
 prodJSON=selectedProduct();
 prodJSON.fromreaction=true;
     window.optNum=document.getElementsByClassName('variant-select-option');
+    window.cidx=0;
     var imgSrc=document.getElementsByClassName('img-responsive')[0].src;
     prodJSON.image=[];
     var imgObj={'url':imgSrc,'name':'mainExternal'};
     prodJSON.image.push(imgObj);
-    for(z=0;z<optNum.length;z++){
-    if(optNum[z]){optNum[z].click()}; 
-    imgSrc=document.getElementsByClassName('img-responsive')[0].src;
-    if(optNum[z]){var imgObj={'url':imgSrc,'name':optNum[z].innerHTML,'isvariant':true};}
-    prodJSON.image.push(imgObj);
-    }
+    processVariants();
 }else if(emporyoumode=="magento"){
     
 }else{console.log('error:unsupported ecommerce')};
 //////////////////////////////////////////////////////////////////////////
 gio={att:function(e,a,v){if(e){if(v){e.setAttribute(a,v);}else{return e.getAttribute(a)}}return false},ins:function(p,tag,aa,_html,b){var i;var elm=document.createElement(tag);if(_html){elm.innerHTML=_html;}if(aa){for(i=0;i<aa.length;i+=2){this.att(elm,aa[i],aa[i+1]);}}if(p){if(b==true){return p.insertBefore(elm,p.firstChild);}else if(b){return p.insertBefore(elm,b);}else{return p.appendChild(elm);}}else{return elm}}}
-var f=gio.ins(document.body,'form',['method','post','target','_blank','action','http://emporyou.com/merchant/metaframe?page=merchant/coupon.html','enctype','multipart/form-data']);
-var i=gio.ins(f,'textarea',['name','jdata'],JSON.stringify(prodJSON));setTimeout('f.submit()',250);
+function finishData(){var f=gio.ins(document.body,'form',['method','post','target','_blank','action','http://emporyou.com/merchant/metaframe?page=merchant/coupon.html','enctype','multipart/form-data']);
+var i=gio.ins(f,'textarea',['name','jdata'],JSON.stringify(prodJSON));setTimeout('f.submit()',250);}
+function processVariants(){
+    var v=optNum[cidx];
+    if(!v){
+        finishData();
+    }else{
+        cidx++;
+        v.click();
+        setTimeout(_processVariants,250)
+    }
+}
+function _processVariants(){
+    var imgSrc=document.getElementsByClassName('img-responsive')[0].src;
+    var imgObj={'url':imgSrc,'name':optNum[z].innerHTML,'isvariant':true};
+    prodJSON.image.push(imgObj);
+    processVariants()
+}
