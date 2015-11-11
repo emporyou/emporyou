48b48505563f30604189a2c013b0dd00ca4122ea
tritone11 <gioele@officinetesla.it>
2015-11-11 18:53:08 +0100

    aaa

--- home/js/coupon.js
+++ home/js/coupon.js
@@ -124,11 +124,11 @@ function sendCoupon() {
             for(var ww=0;ww<initialjdata.variants.length;ww++){
                 if(initialjdata.variants[ww].isMain==true){
                     initialjdata.variants[ww].image=initialjdata.image[p].url;
-                }} 
+                } }
         if(initialjdata.variants[ww].option==initialjdata.image[p].name){
                     initial.variants[ww].image=image[p].url;
                 }
-            }
+            
         }
     }
     myJSON.images=initialjdata.image;
