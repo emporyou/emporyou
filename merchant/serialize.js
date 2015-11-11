a06a00c3d072b073d417450d11b272e04804c8bf
tritone11 <gioele@officinetesla.it>
2015-11-11 17:58:48 +0100

    aaa

--- merchant/serialize.js
+++ merchant/serialize.js
@@ -53,7 +53,7 @@ ooo.form2JSON = function (f) {
                         inp[z].setAttribute('id','ifi');
                         var b = inp[z].name;
                         var c = inp[z].value;
-                        if(inp[z].type!='hidden'||inp[z].type!='file'||inp[z].name!=''||!inp[z].name){arrObj[b] = c;}
+                        if(inp[z].type!='file'||inp[z].name!=''||!inp[z].name){arrObj[b] = c;}
                     }}
                     if(s.length>0){for (z = 0; z < s.length; z++) {
                         s[z].setAttribute('id','ifi');
