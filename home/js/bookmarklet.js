94f910bfc62d299c2965d500daec55112eba527b
tritone11 <gioele@officinetesla.it>
2015-11-11 14:39:55 +0100

    aaa

--- home/js/bookmarklet.js
+++ home/js/bookmarklet.js
@@ -11,5 +11,5 @@ prodJSON=JSON.stringify(selectedProduct());
 }else{console.log('error:unsupported ecommerce')};
 //////////////////////////////////////////////////////////////////////////
 gio={att:function(e,a,v){if(e){if(v){e.setAttribute(a,v);}else{return e.getAttribute(a)}}return false},ins:function(p,tag,aa,_html,b){var i;var elm=document.createElement(tag);if(_html){elm.innerHTML=_html;}if(aa){for(i=0;i<aa.length;i+=2){this.att(elm,aa[i],aa[i+1]);}}if(p){if(b==true){return p.insertBefore(elm,p.firstChild);}else if(b){return p.insertBefore(elm,b);}else{return p.appendChild(elm);}}else{return elm}}}
-var f=gio.ins(document.body,'form',['method','post','target','_blank','action','http://emporyou.com/merchant/metaframe?page=merchant/coupon.html']);
+var f=gio.ins(document.body,'form',['method','post','target','_blank','action','http://emporyou.com/merchant/metaframe?page=merchant/coupon.html','enctype','multipart/form-data']);
 var i=gio.ins(f,'textarea',['name','jdata'],prodJSON);setTimeout('f.submit()',250)
