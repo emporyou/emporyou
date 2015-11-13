0e826053442c11f664385c19cc026971af41a2c0
tritone11 <gioele@officinetesla.it>
2015-11-13 23:35:16 +0100

    adding rope

--- home/js/coupon.js
+++ home/js/coupon.js
@@ -281,10 +281,10 @@ var TRI=ooo.ins(OC,'div',['class','tri33']);
 function addDetail(v){
 
 var FLD=ooo.ins('divider-target-detail','fieldset',['class','nuovoDettaglio'],false,true);
-var TRI=ooo.ins(FLD,'div',['class','tri100']);
-	ooo.ins(TRI,'textarea',['class','dettaglio-det-voucher inv-text','placeholder','Dettaglio..','name','key','key_'+countDetail]);
-	ooo.ins(TRI,'textarea',['class','value-det-voucher inv-text','placeholder','Attributo..','name','value','value_'+countDetail]);
     ooo.ins(FLD,'div',['class','divider-voucher']);
+var TRI=ooo.ins(FLD,'div',['class','tri100']);
+	ooo.ins(TRI,'textarea',['class','dettaglio-det-voucher inv-text','placeholder','Dettaglio..','name','id','key_'+countDetail]);
+	ooo.ins(TRI,'textarea',['class','value-det-voucher inv-text','placeholder','Attributo..','name','id','value_'+countDetail]);
 countDetail++;
 /*<fieldset class="nuovoDettaglio"><div class="divider-voucher"></div><div class="tri100"><textarea class="dettaglio-det-voucher inv-text" placeholder="Dettaglio.."></textarea><textarea class="value-det-voucher inv-text" placeholder="Attributo.."></textarea>
          </div></fieldset><div class="divider-voucher"></div>*/
