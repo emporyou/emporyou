7435ae8e7927a766d833ea8969e3caee53d0eb74
tritone11 <gioele@officinetesla.it>
2015-11-14 00:54:59 +0100

    aaa

--- home/js/coupon.js
+++ home/js/coupon.js
@@ -304,13 +304,14 @@ function calc(){
         var totale_=valbas_-risparmio_
         prefin[pp].value=totale_.toFixed(2);
     }
+    
     //valore-base/100*discount-textarea-valore-base=price-base
     //priceName_0/discount-textarea=priceFinal_0  
 }
 function displayunicode(e){
 var unicode=e.keyCode? e.keyCode : e.charCode
 if (unicode >= 48 && unicode <= 57){
-    calc()
+    if(document.getElementById('discount-textarea').value>99){document.getElementById('discount-textarea').value=99};calc()
 }
 else{
 }
