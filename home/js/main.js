0b67c1d8dc2d73162aa0f27c9e39c95d29524160
tritone11 <gioele@officinetesla.it>
2015-11-15 18:40:26 +0100

    update

--- home/js/main.js
+++ home/js/main.js
@@ -105,7 +105,7 @@ function closeCheckout(){
 function checkRange(xx,yy,cc){
     if(cc){console.log('hai mosso la barra maggiore');
         if(xx.value<yy.value){xx.value=yy.value}
-    }else{console.log('hai mosso la barra minore and '+yy.value+' è il valore della barra sotto');
+    }else{console.log('hai mosso la barra minore and '+xx.value+' è il valore della barra attuale');
         if(xx.value>yy.value){xx.value=yy.value}
     }
 }
