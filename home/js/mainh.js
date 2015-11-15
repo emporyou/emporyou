9b3142fb780327c0b3a3fece5110eb6288dbad36
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-11-15 16:30:06 +0100

    update

--- home/js/mainh.js
+++ home/js/mainh.js
@@ -22,8 +22,7 @@ window.cartTotalShipment=0;
 		var cartXML=getcartXML();
 		var d='none';if(window.cartTotalItems>0){d=''}
 		ooo.$$('bought-container').style.display=d;
-		ooo.$$('flag-text').innnerHTML=window.cartTotalItems;
-		//server_syncart();
+		ooo.$$('flag-text').innerHTML=window.cartTotalItems;
 		window.cartTotalTax=(cartTotalSub/100)*22;
 		window.cartTotalTax=Math.round(window.cartTotalTax*100)/100;
 		window.cartTotalSub=Math.round(window.cartTotalSub*100)/100;
