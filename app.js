9e25d53496aa7f6b4c4def55441f94b85675ae8b
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-12-16 03:11:02 +0100

    update

--- app.js
+++ app.js
@@ -184,6 +184,7 @@ app.get(/^\/testmail\/?.*/,upload.any(),function(req,res,next){
   email='From: emporyou@gmail.com\n'+email;
   //ENCODE
   var base64EncodedEmail=new Buffer(email).toString('base64');
+  console.log(base64EncodedEmail);
   var gmail = google.gmail('v1');
   gmail.users.messages.send({auth: auth,'userId':'me','resource':{'raw':base64EncodedEmail}},function(err,resp){
 	  res.set('Content-Type', 'application/json; charset=utf-8');res.end(JSON.stringify({ex:err}));	  
@@ -191,48 +192,6 @@ app.get(/^\/testmail\/?.*/,upload.any(),function(req,res,next){
 });
 });
 });
-rtrim=function(v){var t=v.replace(/\s+$/g,'');return t;};
-function strtr(str,from,to){
-  var fr = '',
-    i = 0,
-    j = 0,
-    lenStr = 0,
-    lenFrom = 0,
-    tmpStrictForIn = false,
-    fromTypeStr = '',
-    toTypeStr = '',
-    istr = '';
-  var tmpFrom=[];var tmpTo=[];var ret='';var match=false;
-  // Received replace_pairs?
-  // Convert to normal from->to chars
-  if (typeof from === 'object') {
-    tmpStrictForIn = this.ini_set('phpjs.strictForIn', false); // Not thread-safe; temporarily set to true
-    from = this.krsort(from);
-    this.ini_set('phpjs.strictForIn', tmpStrictForIn);
-    for (fr in from) {
-      if (from.hasOwnProperty(fr)) {
-        tmpFrom.push(fr);
-        tmpTo.push(from[fr]);
-    } }
-    from=tmpFrom;to=tmpTo;}
-  // Walk through subject and replace chars when needed
-  lenStr=str.length;lenFrom=from.length;
-  fromTypeStr = typeof from === 'string';
-  toTypeStr = typeof to === 'string';
-  for(i=0;i<lenStr;i++){match=false;
-    if(fromTypeStr){istr=str.charAt(i);
-      for (j = 0; j< lenFrom; j++) {
-        if (istr == from.charAt(j)) {
-          match = true;
-          break;
-    } } }
-	else{for(j=0;j<lenFrom;j++){if(str.substr(i,from[j].length)==from[j]){match=true;i=(i+from[j].length)-1;break;}}}
-    if(match){ret+=toTypeStr ? to.charAt(j):to[j];}
-	else{ret+=str.charAt(i);}
-  }  return ret;
-}
-
-
 //---------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------- STATIC FILES SERVER CONFIGURATION
 //---------------------------------------------------------------------------------------------------
